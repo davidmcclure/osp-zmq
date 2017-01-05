b3295bbac55aadc46dc1ac8f6be64b3e735abe88
David McClure <davidwilliammcclure@gmail.com>
2017-01-05 11:40:16 -0800

    Add a testing extract-text proc for Dask.

---
+++ bin/extract-text.py
@@ -0,0 +1,14 @@
+
+
+from distributed import client
+
+from osp.pipeline import ListWARCPaths, ParseWARC
+
+
+client = Client()
+
+paths = ListWARCPaths.from_env()
+
+parse_warc = ParseWARC.from_env()
+
+docs = client.map(parse_warc, paths)
