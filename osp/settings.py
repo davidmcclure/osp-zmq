6079add16b02f4c0ad1f0fd87fec43de5281dc0d
David McClure <davidwilliammcclure@gmail.com>
2017-01-14 16:38:35 -0800

    Read the scheduler host from .env.

--- osp/settings.py
+++ osp/settings.py
@@ -1,11 +1,21 @@
 
 
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+# Read `.env`.
+load_dotenv(find_dotenv())
+
+
 DATABASE = 'osp.db'
 
 SCRAPER_BUCKET = 'syllascrape'
 
 RESULT_BUCKET = 'osp-pipeline-test'
 
-SCHEDULER_HOST = 'localhost'
+SCHEDULER_HOST = os.environ.get(
+    'SCHEDULER_HOST', 'localhost'
+)
 
 SCHEDULER_PORT = 5557
