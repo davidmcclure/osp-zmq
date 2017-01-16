5c77df72d38c39fe251e830407b4807155ffd73b
David McClure <davidwilliammcclure@gmail.com>
2017-01-15 23:47:26 -0800

    Write text into 'text2'.

--- osp/services/buckets.py
+++ osp/services/buckets.py
@@ -79,7 +79,7 @@ class ResultBucket(Bucket):
             text (str): Extracted text.
         """
         # Form S3 path.
-        path = os.path.join('text', '{}.txt'.format(record_id))
+        path = os.path.join('text2', '{}.txt'.format(record_id))
 
         # Write text.
         key = Key(self.bucket)
